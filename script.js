75bf2ceeb937b1f0f539fb91881b9bd4bb8db868
ShreeramNew <shreerambca1@gmail.com>
2023-03-28 17:01:31 +0530

    cleaned

--- script.js
+++ script.js
@@ -2,14 +2,9 @@ let op = document.querySelectorAll(".atomic-item");
 let numbers = document.querySelectorAll(".numbers");
 let display = document.querySelector("#display");
 let operators = document.querySelectorAll(".operators");
-let tools=document.querySelectorAll(".tools");
+let tools = document.querySelectorAll(".tools");
 let operand1, operand2;
 let final_operator;
-
-str="22+456";
-opIndex=str.indexOf("+");
-console.log(str.substring(str.indexOf("+")+1,str.length));
-
 
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", (event) => {
@@ -32,7 +27,7 @@ for (i = 0; i < numbers.length; i++) {
 for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", (event) => {
         if (event.currentTarget.textContent == "=") {
-            operand2 = parseInt(display.innerHTML.substring(display.innerHTML.indexOf(final_operator)+1,display.innerHTML.length));
+            operand2 = parseInt(display.innerHTML.substring(display.innerHTML.indexOf(final_operator) + 1, display.innerHTML.length));
             switch (final_operator) {
                 case "+": displayText(operand1 + operand2);
                     break;
@@ -49,20 +44,20 @@ for (i = 0; i < operators.length; i++) {
         else {
             operand1 = parseInt(display.innerHTML);
             final_operator = event.currentTarget.textContent;
-            displayText(display.innerHTML+final_operator);
+            displayText(display.innerHTML + final_operator);
         }
     })
 }
-for(i=0;i<tools.length;i++){
-    tools[i].addEventListener("click",(event)=>{
-        switch(event.currentTarget.textContent){
-            case "C":displayText("0");
-            break;
-            case "<--":displayText((display.innerHTML).substring(0,(display.innerHTML).length-1));
-            break;
+for (i = 0; i < tools.length; i++) {
+    tools[i].addEventListener("click", (event) => {
+        switch (event.currentTarget.textContent) {
+            case "C": displayText("0");
+                break;
+            case "<--": displayText((display.innerHTML).substring(0, (display.innerHTML).length - 1));
+                break;
         }
     })
 }
-function displayText(text){
-    display.innerHTML=text;
+function displayText(text) {
+    display.innerHTML = text;
 }
